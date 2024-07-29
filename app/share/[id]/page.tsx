6f45ecf9d692fc7287fe9793f908aec4778efa0b
solanacryptodev <eoscrypto87@protonmail.com>
2024-07-28 23:14:19 -0400

    Build fail fixed

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -1,11 +1,14 @@
 import { type Metadata } from 'next'
-import { notFound, redirect } from 'next/navigation'
+import { notFound } from 'next/navigation'
+import dynamic from 'next/dynamic'
 
 import { formatDate } from '@/lib/utils'
 import { getSharedChat } from '@/app/actions'
-import { ChatList } from '@/components/chat-list'
-import { FooterText } from '@/components/footer'
-import { AI, UIState, getUIStateFromAIState } from '@/lib/chat/actions'
+import { UIState, getUIStateFromAIState } from '@/lib/chat/actions'
+
+const ChatList = dynamic(() => import('@/components/chat-list').then(mod => mod.ChatList))
+const FooterText = dynamic(() => import('@/components/footer').then(mod => mod.FooterText))
+const AI = dynamic(() => import('@/lib/chat/actions').then(mod => mod.AI))
 
 export const runtime = 'edge'
 export const preferredRegion = 'home'
@@ -17,8 +20,8 @@ interface SharePageProps {
 }
 
 export async function generateMetadata({
-  params
-}: SharePageProps): Promise<Metadata> {
+                                         params
+                                       }: SharePageProps): Promise<Metadata> {
   const chat = await getSharedChat(params.id)
 
   return {
@@ -36,23 +39,23 @@ export default async function SharePage({ params }: SharePageProps) {
   const uiState: UIState = getUIStateFromAIState(chat)
 
   return (
-    <>
-      <div className="flex-1 space-y-6">
-        <div className="border-b bg-background px-4 py-6 md:px-6 md:py-8">
-          <div className="mx-auto max-w-2xl">
-            <div className="space-y-1 md:-mx-8">
-              <h1 className="text-2xl font-bold">{chat.title}</h1>
-              <div className="text-sm text-muted-foreground">
-                {formatDate(chat.createdAt)} · {chat.messages.length} messages
+      <>
+        <div className="flex-1 space-y-6">
+          <div className="border-b bg-background px-4 py-6 md:px-6 md:py-8">
+            <div className="mx-auto max-w-2xl">
+              <div className="space-y-1 md:-mx-8">
+                <h1 className="text-2xl font-bold">{chat.title}</h1>
+                <div className="text-sm text-muted-foreground">
+                  {formatDate(chat.createdAt)} · {chat.messages.length} messages
+                </div>
               </div>
             </div>
           </div>
+          <AI>
+            <ChatList messages={uiState} isShared={true} />
+          </AI>
         </div>
-        <AI>
-          <ChatList messages={uiState} isShared={true} />
-        </AI>
-      </div>
-      <FooterText className="py-8" />
-    </>
+        <FooterText className="py-8" />
+      </>
   )
 }
