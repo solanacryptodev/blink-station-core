4392308483b468e59641111eeb21037ab024f81d
solanacryptodev <eoscrypto87@protonmail.com>
2024-07-29 10:54:37 -0400

    All .env keys now have to go through next config

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 import { Connection, PublicKey } from "@solana/web3.js";
-const newConnection = process.env.NEXT_PUBLIC_HELIUS_RPC_URL as string;
+const newConnection = process.env.HELIUS_RPC_URL as string;
 
 export const ATLAS: string = 'ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx';
 export const PROGRAM_ID = new PublicKey('traderDnaR5w6Tcoi3NFm53i48FTDNbGjBSZwWXDRrg');
