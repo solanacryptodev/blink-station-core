8809062450f6567e5b06b32835261c4a99f1fed6
solanacryptodev <eoscrypto87@protonmail.com>
2024-07-28 23:49:09 -0400

    Build fail fixed

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,6 @@ import { authConfig } from './auth.config'
 
 export default NextAuth(authConfig).auth
 
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
-}
+// export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+// }
