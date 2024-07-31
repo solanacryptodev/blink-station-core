0e6922835a45858b51f513ad74f9537e6a53f5bd
solanacryptodev <eoscrypto87@protonmail.com>
2024-07-31 10:10:43 -0400

    Attempted blink fix for opengraph metadata 2

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -5,8 +5,8 @@ export const GET = async () => {
         rules: [
             // map all root level routes to an action
             {
-                pathPattern: "/blink",
-                apiPath: "/api/actions/buy",
+                pathPattern: "/blink?asset=:assetId",
+                apiPath: "/api/actions/buy?asset=:assetId",
             },
             // fallback path
             // TODO: fetch quantity from on-chain. Currently hardcoded to 618.
