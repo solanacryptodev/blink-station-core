b27bbbd777708b3db92d7a5bdac67cdbd164ee40
solanacryptodev <eoscrypto87@protonmail.com>
2024-08-02 20:57:55 -0400

    Finally fixed this bug and got to the root of what caused it lol

--- presenters/OrdersPresenter.ts
+++ presenters/OrdersPresenter.ts
@@ -1,7 +1,7 @@
-import { makeAutoObservable, runInAction, observable, action, autorun, computed } from 'mobx';
+import { makeObservable, runInAction, observable, action, autorun, toJS } from 'mobx';
 import { ReturnedOrders } from "@/lib/types";
 import { GmClientService } from "@staratlas/factory";
-import { bnToNumber, formatOrderNumber, getNftMint, getNftName, getNftParam, removeDecimal } from "@/lib/utils";
+import { bnToNumber, getNftMint, getNftName } from "@/lib/utils";
 import { PublicKey } from "@solana/web3.js";
 import { ATLAS, CONNECTION, PROGRAM_ID } from "@/lib/constants";
 import { BN } from "@coral-xyz/anchor";
@@ -13,7 +13,6 @@ export class OrdersPresenter {
     private static instance: OrdersPresenter | null = null;
     orders: ReturnedOrders[];
     isLoading: boolean;
-    isFetchingOrders: boolean;
     isFetchComplete: boolean;
     error: string | null;
     blinkURL: string;
@@ -21,104 +20,75 @@ export class OrdersPresenter {
     constructor() {
         this.orders = [];
         this.isLoading = false;
-        this.isFetchingOrders = false;
         this.isFetchComplete = false;
         this.error = '';
         this.blinkURL = '';
 
-        makeAutoObservable(this, {
+        makeObservable(this, {
             orders: observable,
             isLoading: observable,
-            isFetchingOrders: observable,
             isFetchComplete: observable,
             error: observable,
             blinkURL: observable,
 
             fetchOrders: action.bound,
             buildBlinkUrl: action.bound,
-
-            fetchStatus: computed
+            reset: action.bound
         });
+    }
+
+    reset() {
+        this.orders = [];
+        this.isLoading = false;
+        this.isFetchComplete = false;
+        this.error = null;
+        this.blinkURL = '';
     }
 
     componentDidMount() {
         autorun(() => {
-            if ( !this.fetchStatus.isFetchingOrders ) {
-                // this.fetchOrders();
-                runInAction(() => {
-
-                })
-            } else {
-                runInAction(() => {
-
-                })
-            }
-            // console.log('lore component mounted...', toJS(this.lore));
+            console.log('componentDidMount... ');
+            console.log('states...', {
+                orders: toJS(this.orders),
+                isLoading: this.isLoading,
+                isFetchComplete: this.isFetchComplete,
+                error: this.error
+            })
         });
     }
 
     static getInstance(): OrdersPresenter {
         if (!OrdersPresenter.instance) {
             OrdersPresenter.instance = new OrdersPresenter();
-            // console.log('OrdersPresenter instance created...', OrdersPresenter.instance);
         }
         return OrdersPresenter.instance;
     }
 
-    get fetchStatus() {
-        return {
-            isLoading: this.isLoading,
-            isFetchingOrders: this.isFetchingOrders,
-            isFetchComplete: this.isFetchComplete
-        }
-    }
-
-    async fetchOrders(assetName: string, ownerKey: string): Promise<void> {
-        this.isLoading = true;
-        this.error = null;
-
+    async fetchOrders(assetName: string, ownerKey: string): Promise<ReturnedOrders[]> {
         try {
             const gmClientService = new GmClientService();
             const nftMint = getNftMint(assetName) as PublicKey;
             const name = getNftName(assetName) as string;
-            const paramName = getNftParam(assetName) as string;
-            // console.log('asset name is... ', assetName);
-            // console.log('nftMint: ', nftMint.toString());
-            // console.log('name: ', name);
 
             const orders = await gmClientService.getOpenOrdersForAsset(CONNECTION, nftMint, PROGRAM_ID);
-            const sellOrders = orders.filter(order => order.orderType === 'sell');
-            const userOrders = sellOrders.filter(order => order.owner.toString() === ownerKey);
-            // console.log('user orders unfiltered... ', userOrders);
+            const playerSellOrders = orders.filter(order => order.orderType === 'sell');
+            const playerOrders = playerSellOrders.filter(order => order.owner.toString() === ownerKey);
 
-            if (userOrders.length === 0) {
-                this.error = 'No open orders found for this user in this market.';
-                return;
+            if (playerOrders.length === 0) {
+                throw new Error('No open orders found for this user in this market.');
             }
 
-            const openOrders: ReturnedOrders[] = userOrders.map(order => ({
+            return playerOrders.map(order => ({
                 assetName: name.toLowerCase(),
-                assetParam: paramName,
                 orderType: order.orderType,
                 orderId: order.id.toString(),
-                price: formatOrderNumber(new BN(order.price), order),
+                price: bnToNumber(new BN(order.price)),
                 quantity: order.orderQtyRemaining,
                 owner: order.owner.toString(),
-                currency: order.currencyMint === ATLAS ? 'ATLAS' : 'USDC'
+                currency: order.orderMint === ATLAS ? 'ATLAS' : 'USDC'
             }));
-            // console.log('openOrders: ', openOrders);
-
-            runInAction(() => {
-                this.orders.push(...openOrders);
-                // console.log('this.orders... ', toJS((this.orders)));
-                this.isFetchComplete = true;
-                this.isLoading = false;
-            });
         } catch (error) {
-            runInAction(() => {
-                this.error = error as string;
-                this.isLoading = false;
-            });
+            throw error;
         }
     }
 
@@ -130,26 +100,19 @@ export class OrdersPresenter {
                 const name = assets.filter((asset) => asset.mint === getOrder.orderMint);
                 this.orders.push({
                     assetName: name[0].name.toLowerCase(),
-                    assetParam: name[0].param,
                     orderType: getOrder.orderType,
                     orderId: getOrder.id.toString(),
                     price: bnToNumber(new BN(getOrder.price)),
                     quantity: getOrder.orderQtyRemaining,
-                    owner: getOrder.owner.toString(),
-                    currency: getOrder.currencyMint === ATLAS ? 'ATLAS' : 'USDC'
+                    owner: getOrder.owner.toString()
                 });
             }
 
             const orders = this.orders.filter(order => order.orderId === orderID);
             // console.log('orders: ', orders);
-            const removedDecimal = removeDecimal(orders[0].price as number);
 
             runInAction(() => {
-                if (orders[0].currency === 'ATLAS') {
-                    this.blinkURL = `https://blinkstationx.com/blink?asset=${orders[0].assetParam}|${orders[0].orderId}|${removedDecimal}|${orders[0].quantity}|${orders[0].currency?.toLowerCase()}/`;
-                } else {
-                    this.blinkURL = `https://blinkstationx.com/blink?asset=${orders[0].assetParam}|${orders[0].orderId}|${orders[0].price}|${orders[0].quantity}|${orders[0].currency?.toLowerCase()}/`;
-                }
+                this.blinkURL = `https://blinkstationx.com/blink?asset=${orders[0].assetName}|${orders[0].orderId}|${orders[0].price}|${orders[0].quantity}/`;
                 // console.log('blinkURL: ', this.blinkURL);
             });
 
