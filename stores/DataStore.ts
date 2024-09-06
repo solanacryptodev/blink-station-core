3bd4b87d7344d89c890a323ed6fc11ceb97d575a
Brian Lewis <52287356+solanacryptodev@users.noreply.github.com>
2024-09-06 13:08:53 -0400

    Example asset analysis (#37)
    * Changed mock data for free users and adjusted flipside crypto data to change performance
    
    * Changed mock data for free users and adjusted flipside crypto data to change performance
    
    * Changed mock data for free users and adjusted flipside crypto data to change performance

--- stores/DataStore.ts
+++ stores/DataStore.ts
@@ -1,20 +1,21 @@
 import { makeObservable } from "mobx";
 import {
-    totalBuyAndSellQuantities,
-    totalBuyAndSellPrices,
-    totalAssetExchanges,
+    averageSellPrice,
     lowestCurrentPrice,
-    averageSellPrice } from '@/app/data-actions';
+    totalAssetExchanges,
+    totalBuyAndSellPrices,
+    totalBuyAndSellQuantities
+} from '@/app/data-actions';
 import { RootStore } from "@/stores/RootStore";
-import NodeCache from "node-cache";
+// import NodeCache from "node-cache";
 
 export class DataStore {
-    private cache: NodeCache;
+    // private cache: NodeCache;
     rootStore: RootStore;
 
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore;
-        this.cache = new NodeCache({ stdTTL: 600 });
+        // this.cache = new NodeCache({ stdTTL: 600 });
 
         makeObservable(this, {})
     }
@@ -42,16 +43,15 @@ export class DataStore {
 
     async totalAssetExchanges(mint: string, currency: string) {
         const cacheKey = `totalAssetExchanges-${mint}-${currency}`;
-        const cachedResult = this.cache.get<number>(cacheKey);
-
-        if (cachedResult !== undefined) {
-            return cachedResult;
-        }
+        // const cachedResult = this.cache.get<number>(cacheKey);
+        //
+        // if (cachedResult !== undefined) {
+        //     return cachedResult;
+        // }
 
         try {
-            const result = await totalAssetExchanges(mint, currency);
-            this.cache.set(cacheKey, result);
-            return result;
+            // this.cache.set(cacheKey, result);
+            return await totalAssetExchanges( mint, currency );
         } catch (error) {
             console.error('Error in DataStore.totalAssetExchanges:', error);
             throw error;
